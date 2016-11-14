796aa2a71c9255528778397f2180c605a926bd96
Luke Wilson <wilson.palooza@gmail.com>
2016-11-14 11:43:14 -0800

    - Imports ProfileComponent - Exports profileState at url /profile

--- app/states.ts
+++ app/states.ts
@@ -1,8 +1,10 @@
 import {Home} from "./components/home/home";
 import { ProductsComponent } from "./components/products/products.component";
+import { ProfileComponent } from "./components/profile/profile.component";
 import {Transition} from "ui-router-ng2";
 
 /** States */
 export const homeState = { name: 'home', url: '/',  component: Home };
 
 export const gearState = { name: 'gear', url: '/gear',  component: ProductsComponent };
+export const profileState = { name: 'profile', url: '/profile',  component: ProfileComponent };
