7bcd26bafaaa6f9a03c4e74ccda90c1c622f6326
Luke Wilson <wilson.palooza@gmail.com>
2016-11-22 10:43:38 -0800

    - Add initial code to get google map rendering properly on products page (products.ts)

--- app/components/products/products.ts
+++ app/components/products/products.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit, ViewChild, ElementRef }   from "@angular/core";
-import { FormControl }                     from "@angular/forms";
+import { FormControl }         from "@angular/forms";
 import { MapsAPILoader }       from "angular2-google-maps/core";
 import { NgbRatingConfig }     from "@ng-bootstrap/ng-bootstrap";
 import { ProductsService }     from "./products.service";
 import { UIROUTER_DIRECTIVES } from "ui-router-ng2";
+
+interface marker {
+    place: string;
+    label?: string;
+    draggable: boolean;
+    icon?: string;
+}
 
 @Component({
   moduleId: module.id,
@@ -15,10 +22,21 @@ import { UIROUTER_DIRECTIVES } from "ui-router-ng2";
 
 export class Products implements OnInit {
   public products: Array<any>;
-  public latitude: number;
-  public longitude: number;
+  public lat: number = -37.7863713;
+  public lng: number = 175.2796333;
   public searchControl: FormControl;
-  public zoom: number;
+  public zoom: number = 4;
+
+  public markers: marker[] = [
+    {
+      place: "ChIJJ1XCysTQmoARU9qxtqimyiE",
+      draggable: false
+    },
+    {
+      place: "ChIJtaxkMdvQmoARA8Diy_MifCo",
+      draggable: false
+    }
+  ];
 
   // Note: This is looking for #search in the HTML template
   @ViewChild("search")
@@ -31,6 +49,10 @@ export class Products implements OnInit {
   ) {
     config.max = 5;
     config.readonly = true;
+  }
+
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`)
   }
 
   public getProducts() {
@@ -57,28 +79,28 @@ export class Products implements OnInit {
   public ngOnInit(): void {
     this.getProducts();
     // set google maps defaults
-    this.zoom = 4;
-    this.latitude = 39.8282;
-    this.longitude = -98.5795;
+    // this.zoom = 4;
+    // this.latitude = 39.8282;
+    // this.longitude = -98.5795;
 
     // create search FormControl
-    this.searchControl = new FormControl();
+    // this.searchControl = new FormControl();
 
     // set current position
-    this.setCurrentPosition();
+    // this.setCurrentPosition();
 
     // load Places Autocomplete
-    this.mapsAPILoader.load()
+    // this.mapsAPILoader.load()
 
   }
 
-  private setCurrentPosition() {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.latitude = position.coords.latitude;
-        this.longitude = position.coords.longitude;
-        this.zoom = 12;
-      });
-    }
-  }
+  // private setCurrentPosition() {
+  //   if ("geolocation" in navigator) {
+  //     navigator.geolocation.getCurrentPosition((position) => {
+  //       this.latitude = position.coords.latitude;
+  //       this.longitude = position.coords.longitude;
+  //       this.zoom = 12;
+  //     });
+  //   }
+  // }
 }
