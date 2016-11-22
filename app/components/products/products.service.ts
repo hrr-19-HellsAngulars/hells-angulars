e0083b439fe7e56ad6e9b4868e3f529d93f9c6e4
Alex Leo <aleo1337@gmail.com>
2016-11-22 17:32:17 -0500

    Fix url in getProductsByQuery function

--- app/components/products/products.service.ts
+++ app/components/products/products.service.ts
@@ -23,7 +23,7 @@ export class ProductsService {
   }
 
   public getProductsByQuery(): Promise<any> {
-    let url = "/api/products?productname=" + this.keyword;
+    let url = "/api/products/search?query=" + this.keyword;
     return this.http.get(url)
                     .toPromise()
                     .then(response => response.json())
