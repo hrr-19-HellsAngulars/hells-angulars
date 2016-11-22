0610148bb19232e9540d23470777e68a05f8b151
Luke Wilson <wilson.palooza@gmail.com>
2016-11-21 16:13:06 -0800

    - Add code to render google map on products page in products.ts

--- app/components/products/products.ts
+++ app/components/products/products.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit }   from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef }   from "@angular/core";
+import { FormControl }                     from "@angular/forms";
+import { MapsAPILoader }       from "angular2-google-maps/core";
 import { NgbRatingConfig }     from "@ng-bootstrap/ng-bootstrap";
 import { ProductsService }     from "./products.service";
 import { UIROUTER_DIRECTIVES } from "ui-router-ng2";
@@ -13,8 +15,17 @@ import { UIROUTER_DIRECTIVES } from "ui-router-ng2";
 
 export class Products implements OnInit {
   public products: Array<any>;
+  public latitude: number;
+  public longitude: number;
+  public searchControl: FormControl;
+  public zoom: number;
+
+  // Note: This is looking for #search in the HTML template
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
 
   constructor(
+    private mapsAPILoader: MapsAPILoader,
     private productsService: ProductsService,
     private config: NgbRatingConfig
   ) {
@@ -45,5 +56,29 @@ export class Products implements OnInit {
 
   public ngOnInit(): void {
     this.getProducts();
+    // set google maps defaults
+    this.zoom = 4;
+    this.latitude = 39.8282;
+    this.longitude = -98.5795;
+
+    // create search FormControl
+    this.searchControl = new FormControl();
+
+    // set current position
+    this.setCurrentPosition();
+
+    // load Places Autocomplete
+    this.mapsAPILoader.load()
+
+  }
+
+  private setCurrentPosition() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 12;
+      });
+    }
   }
 }
