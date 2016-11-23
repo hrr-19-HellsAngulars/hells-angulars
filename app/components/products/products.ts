310776f8becc2dd04e725baa27b31e58b966495b
Luke Wilson <wilson.palooza@gmail.com>
2016-11-23 11:23:45 -0800

    Change code to handle new product object (now containing lat and lng variables) so that markers can be rendered on the google map

--- app/components/products/products.ts
+++ app/components/products/products.ts
@@ -5,13 +5,6 @@ import { NgbRatingConfig }     from "@ng-bootstrap/ng-bootstrap";
 import { ProductsService }     from "./products.service";
 import { UIROUTER_DIRECTIVES } from "ui-router-ng2";
 
-interface Marker {
-    place: string;
-    label?: string;
-    draggable: boolean;
-    icon?: string;
-}
-
 @Component({
   moduleId: module.id,
   providers: [ NgbRatingConfig ],
@@ -22,21 +15,11 @@ interface Marker {
 
 export class Products implements OnInit {
   public products: Array<any>;
+  public markers: Array<any>;
   public lat: number = -37.7863713;
   public lng: number = 175.2796333;
   public searchControl: FormControl;
   public zoom: number = 4;
-
-  public markers: Marker[] = [
-    {
-      place: "ChIJJ1XCysTQmoARU9qxtqimyiE",
-      draggable: false,
-    },
-    {
-      place: "ChIJtaxkMdvQmoARA8Diy_MifCo",
-      draggable: false,
-    },
-  ];
 
   // Note: This is looking for #search in the HTML template
   @ViewChild("search")
@@ -60,47 +43,48 @@ export class Products implements OnInit {
         .getProductsByQuery()
         .then(products => {
           // Rearrange products to have 3 products in one row
+          let allProducts: Array<any> = [];
           let productsWithRows: Array<any> = [];
           let row: Array<any> = [];
           for (let i = 0; i < products.length; i++) {
+            allProducts.push(products[i]);
             if (i % 3 === 0 && row.length > 0) {
               productsWithRows.push(row);
               row = [];
             }
+
             row.push(products[i]);
             if (i === products.length - 1) {
               productsWithRows.push(row);
             }
           }
           this.products = productsWithRows;
+          this.markers = allProducts;
         });
   }
 
   public ngOnInit(): void {
     this.getProducts();
-    // set google maps defaults
-    // this.zoom = 4;
-    // this.latitude = 39.8282;
-    // this.longitude = -98.5795;
 
-    // create search FormControl
-    // this.searchControl = new FormControl();
+    // set google maps defaults
+    this.zoom = 15;
+    this.latitude = -39.8282;
+    this.longitude = 111.5795;
 
     // set current position
-    // this.setCurrentPosition();
+    this.setCurrentPosition();
 
     // load Places Autocomplete
-    // this.mapsAPILoader.load()
-
+    this.mapsAPILoader.load()
   }
 
-  // private setCurrentPosition() {
-  //   if ("geolocation" in navigator) {
-  //     navigator.geolocation.getCurrentPosition((position) => {
-  //       this.latitude = position.coords.latitude;
-  //       this.longitude = position.coords.longitude;
-  //       this.zoom = 12;
-  //     });
-  //   }
-  // }
+  private setCurrentPosition() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 12;
+      });
+    }
+  }
 }
