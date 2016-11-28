3718ade2eafb829b1eb10c34884013d8e2318213
Daisy Good <daisy.m.good@gmail.com>
2016-11-28 14:03:27 -0600

    Delete config files

--- app/auth/auth.config.ts
+++
@@ -1,8 +0,0 @@
-interface AuthConfiguration {
-  clientID: string;
-  domain: string;
-}
-export const myConfig: AuthConfiguration = {
-    clientID: "qLqXTQfFnKadSDKoMdb2OqHAqreCLX1P",
-    domain: "prawdigee.auth0.com",
-};
