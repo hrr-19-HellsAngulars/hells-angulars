4442d1a839fea914cb6bdf02a7d3acf995366921
Daisy Good <daisy.m.good@gmail.com>
2016-11-29 15:20:54 -0600

    Add multiple pictures to backend

--- app/components/add_modal/productForm.component.ts
+++ app/components/add_modal/productForm.component.ts
@@ -41,15 +41,22 @@ export class NewProductForm {
   public upload(fileInput: any) {
     let context = this;
     let AWSService = (<any> window).AWS;
-    let file = fileInput.target.files[0];
+    let pictures = [fileInput.target.files[0],fileInput.target.files[1],fileInput.target.files[2], fileInput.target.files[3]]
     AWSService.config.accessKeyId = amazonS3Config.accessKeyId;
     AWSService.config.secretAccessKey = amazonS3Config.secretAccessKey;
     let bucket = new AWSService.S3({params: {Bucket: "gear-box"}});
-    let params = {Key: file.name, Body: file};
-    bucket.upload(params, function (error: any, result: any) {
-      if (error) { console.log(error); };
-      context.model.imageLink = result.Location;
-    });
+    console.log(pictures);
+    pictures.forEach((pic:any, index:number) => {
+      if (pic !== undefined) {
+        let params = {Key: pic.name, Body: pic}
+        bucket.upload(params, function (error: any, result: any) {
+          if (error) { console.log(error); };
+          let thisImageLink = "imageLink" + index;
+          context.model[thisImageLink] = result.Location;
+        });
+      }
+    })
+    
   }
 
   public onSubmit(model: NewProduct) {
