9602ae6529e42df31793e0b444f9a85d1cc04aa4
Luke Wilson <wilson.palooza@gmail.com>
2016-11-30 14:27:46 -0800

    Fix call to editProductService.editProduct by passing it the model. Also, add id property to model and give it the value from the product Import MapsAPILoader in preparation for autocomplete for location

--- app/components/edit-product/edit-product.component.ts
+++ app/components/edit-product/edit-product.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input, Output, EventEmitter, OnInit } from "@angular/core";
+import { MapsAPILoader }                 from "angular2-google-maps/core";
+
 import { NewProduct }    from "../add_modal/newProduct";
 import { EditProductService } from "./edit-product.service";
-
-
 
 @Component({
   moduleId: module.id,
@@ -28,25 +28,17 @@ export class EditProductForm {
   ) { }
 
   public onSubmit(model: NewProduct) {
-    console.log(this.product);
-    console.log("submitted");
-    // model.authorId = this.userId;
-    // model.transactionId = this.transaction.id;
-    // model.productId = this.transaction.product_id;
-    // model.buyerId = this.transaction.buyer_id;
-    // model.sellerId = this.transaction.seller_id;
-    // model.rating = this.selected;
-    // this.addReivewService.addReivew(model)
-    //     .then(result => {
-    //       this.close.emit();
-    //     })
-    //     .catch(error => {
-    //       console.log(error);
-    //     });
+    this.editProductService.editProduct(model)
+        .then(result => {
+          this.close.emit();
+        })
+        .catch(error => {
+          console.log(error);
+        });
   }
 
   ngOnInit(): void {
-    console.log(this.product);
+    this.model.id = this.product.id;
     this.model.productName = this.product.productname;
     this.model.productDescription = this.product.description;
     this.model.pricePerDay = this.product.priceperday;
