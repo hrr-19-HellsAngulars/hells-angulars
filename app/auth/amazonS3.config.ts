009f01dd47481b53d53ad041742ac5731540c17b
Daisy Good <daisy.m.good@gmail.com>
2016-11-30 19:43:42 -0600

    Add process.env

---
+++ app/auth/amazonS3.config.ts
@@ -0,0 +1,8 @@
+interface AmazonS3Config {
+    accessKeyId: string;
+    secretAccessKey: string;
+}
+export const amazonS3Config: AmazonS3Config = {
+    accessKeyId: "AKIAJO6XKSBA5K4EGRYA",
+    secretAccessKey: "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",
+};
