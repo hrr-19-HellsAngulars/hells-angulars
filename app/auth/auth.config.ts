bf3f770023b4ea14a970c6234e49730c9c83bd73
Daisy Good <daisy.m.good@gmail.com>
2016-11-30 19:48:15 -0600

    Add config files

---
+++ app/auth/auth.config.ts
@@ -0,0 +1,8 @@
+interface AuthConfiguration {
+    clientID: string,
+    domain: string
+}
+export const myConfig: AuthConfiguration = {
+    clientID: 'qLqXTQfFnKadSDKoMdb2OqHAqreCLX1P',
+    domain: 'prawdigee.auth0.com'
+};
